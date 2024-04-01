d1a99596d3f01caf31cc8a384e4bca0709bf39f7
Jakub Molinski <jakubmolinski9@gmail.com>
2024-04-01 12:34:27 +0200

    Initial commit - created all necessary files

---
+++ script.js
@@ -0,0 +1,9 @@
+// Create camelize function
+// background-color => backgroundColor
+// split given words
+// transfrom the words properly
+// join them back together
+
+const example = "background-color";
+
+const camelize = (string) => {};
