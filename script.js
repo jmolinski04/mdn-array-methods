154728e1f34857d8778cc4ef0bc37de464d80153
Jakub Molinski <jakubmolinski9@gmail.com>
2024-04-01 15:21:22 +0200

    Map to objects exercise

--- script.js
+++ script.js
@@ -13,11 +13,25 @@
 // console.log(arr);
 // console.log(sorted);
 
-let john = { name: "John", age: 25 };
-let pete = { name: "Pete", age: 30 };
-let mary = { name: "Mary", age: 28 };
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 28 };
 
-let users = [john, pete, mary];
+// let users = [john, pete, mary];
 
-let names = users.map((user) => user.name).join(", ");
-console.log(names);
+// let names = users.map((user) => user.name).join(", ");
+// console.log(names);
+
+// let john = { name: "John", surname: "Smith", id: 1 };
+// let pete = { name: "Pete", surname: "Hunt", id: 2 };
+// let mary = { name: "Mary", surname: "Key", id: 3 };
+
+// let users = [john, pete, mary];
+
+// let usersMapped = users.map((user) => ({
+//   fullName: `${user.name} ${user.surname}`,
+//   id: user.id,
+// }));
+
+// console.log(usersMapped[1].id);
+// console.log(usersMapped[1].fullName);
