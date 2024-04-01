788ad28f14da18a55217047e1728c31f1bcddf79
Jakub Molinski <jakubmolinski9@gmail.com>
2024-04-01 15:00:32 +0200

    First sorting exercise

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-// Write a function filterRange(arr, a, b) that gets an array arr,
-//looks for elements with values higher or equal to a and lower or equal to b and return a result as an array.
-//The function should not modify the array. It should return the new array.
+let arr = [5, 2, 1, -10, 8];
 
-let arr = [5, 3, 8, 1];
-
-const filterRange = (arr, a, b) => {
-  const filtered = arr.filter((value) => {
-    if (a <= value && b >= value) return value;
-  });
-
-  return filtered;
-};
-
-let filtered = filterRange(arr, 1, 4);
-console.log(filtered);
-console.log(arr);
+const sortedArray = arr.sort((a, b) => (a > b ? -1 : 1));
+console.log(sortedArray);
