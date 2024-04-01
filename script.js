df72ee4fd3885bc7915bff4127563dc8b795a539
Jakub Molinski <jakubmolinski9@gmail.com>
2024-04-01 15:09:55 +0200

    Map to names task

--- script.js
+++ script.js
@@ -3,12 +3,21 @@
 // const sortedArray = arr.sort((a, b) => (a > b ? -1 : 1));
 // console.log(sortedArray);
 
-let arr = ["HTML", "JavaScript", "CSS"];
+// let arr = ["HTML", "JavaScript", "CSS"];
 
-const copySorted = (arr) => {
-  return arr.map((x) => x).sort((a, b) => (a > b ? 1 : -1));
-};
+// const copySorted = (arr) => {
+//   return arr.map((x) => x).sort((a, b) => (a > b ? 1 : -1));
+// };
 
-const sorted = copySorted(arr);
-console.log(arr);
-console.log(sorted);
+// const sorted = copySorted(arr);
+// console.log(arr);
+// console.log(sorted);
+
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let users = [john, pete, mary];
+
+let names = users.map((user) => user.name).join(", ");
+console.log(names);
