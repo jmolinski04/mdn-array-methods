8a562b178f22e4535f04bda53cc022dcd2ebcd65
Jakub Molinski <jakubmolinski9@gmail.com>
2024-04-01 15:04:38 +0200

    Second sorting exercise

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-let arr = [5, 2, 1, -10, 8];
+// let arr = [5, 2, 1, -10, 8];
 
-const sortedArray = arr.sort((a, b) => (a > b ? -1 : 1));
-console.log(sortedArray);
+// const sortedArray = arr.sort((a, b) => (a > b ? -1 : 1));
+// console.log(sortedArray);
+
+let arr = ["HTML", "JavaScript", "CSS"];
+
+const copySorted = (arr) => {
+  return arr.map((x) => x).sort((a, b) => (a > b ? 1 : -1));
+};
+
+const sorted = copySorted(arr);
+console.log(arr);
+console.log(sorted);
