ea47f47c4f88c29ead5efe970c4f38c65d390a49
Jakub Molinski <jakubmolinski9@gmail.com>
2024-04-02 10:23:31 +0200

    Finished sorting users by age exercise

--- script.js
+++ script.js
@@ -35,3 +35,16 @@
 
 // console.log(usersMapped[1].id);
 // console.log(usersMapped[1].fullName);
+
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let arr = [pete, john, mary];
+
+const sortByAge = (arr) => {
+  return arr.sort((a, b) => (a.age > b.age ? 1 : -1));
+};
+
+sortByAge(arr);
+console.table(arr);
